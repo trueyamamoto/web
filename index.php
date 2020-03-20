b2ed35ea01cf8d854c35030a81d9594c3bcfb607
trueyamamoto <62404220+trueyamamoto@users.noreply.github.com>
2020-03-20 21:17:27 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,18 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <meta charset='utf-8'>
-  <?php
-    $json_url = "URL"; # URL for getting JSON data
-    $crl = curl_init();
-    curl_setopt($crl, CURLOPT_URL, $json_url);
-    curl_setopt($crl, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($crl, CURLOPT_SSL_VERIFYPEER, FALSE); 
-    $php_json = curl_exec($crl);
-    curl_close($crl);
-  ?>
-  <script>
-    var js_json = <?php echo $php_json; ?>;
-    console.log(js_json["items"][0]["example"]); // refered value in the JSON data
-  </script>
-</head>
-<body>
-  <br>
-</body>
+  <head>
+    <meta charset='utf-8'>
+    <?php
+      $json_url = "URL"; # URL for getting JSON data
+      $php_json = file_get_contents($json_url);
+      # $php_arr = json_decode($php_json, true); if convert JSON to array on PHP
+    ?>
+    <script>
+      var js_json = <?php echo $php_json; ?>;
+      console.log(js_json["items"][0]["example"]); // refered value in the JSON data
+    </script>
+  </head>
+  <body>
+    <br>
+  </body>
 </html>
