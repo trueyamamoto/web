f5d302d39a67a8cdc5cda7aaa8fba494e532f3ae
trueyamamoto <62404220+trueyamamoto@users.noreply.github.com>
2020-03-29 06:40:31 +0900

    Update plotly.py

--- plotly.py
+++ plotly.py
@@ -1,6 +1,5 @@
 """
 # apt install libatlas-base-dev
-# pip3 install pandas 
 # pip3 install plotly 
 # pip3 install cufflinks 
   sign up plot.ly and generate API key
@@ -18,8 +17,13 @@ https://plotly.com/python-api-reference/generated/plotly.graph_objects.Layout.ht
 """
 
 
+import cufflinks as cf
 import plotly as py
 from plotly import graph_objs as go
+
+# data processing
+
+cf.go_offline()
 
 trace1 = go.Scatter(
   ...,
@@ -38,3 +42,5 @@ layout = go.Layout(
 )
 
 fig = go.Figure(data=data, layout=layout)
+
+py.offline.plot(fig, filename='foo.html')
